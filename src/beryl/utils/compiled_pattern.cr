b9f33548d27284c6393d4b9cf64e8cf0c61c7644
Luis Lavena <luislavena@gmail.com>
2015-08-24 20:06:51 -0300

    docs: CompiledPattern

--- src/beryl/utils/compiled_pattern.cr
+++ src/beryl/utils/compiled_pattern.cr
@@ -2,7 +2,44 @@ require "cgi"
 
 module Beryl
   module Utils
+    # Helper object that provides basic pattern matching and extraction of
+    # information from defined named segments.
+    #
+    # Example:
+    #
+    # ```
+    # compiled = CompiledPattern.new("/products/:category/:id")
+    # request  = HTTP::Request.new("GET", "/products/furniture/456-chair")
+    #
+    # compiled.matches?(request)
+    # # => true
+    # params = compiled.extract_params(request)
+    # # => { "category" => "furniture", "id" => "456-chair" }
+    # ```
+    #
+    # It will also extract information given as query parameters in the URL:
+    #
+    # ```
+    # compiled = CompiledPattern.new("/products")
+    # request  = HTTP::Request.new("GET", "/products?sort=date")
+    #
+    # params = compiled.extract_params(request)
+    # # => { "sort" => "date" }
+    # ```
     struct CompiledPattern
+      # Accepts an `String` as pattern.
+      #
+      # It compiles the pattern as regular expression (`Regex`) which later
+      # will be used for matching purposes.
+      #
+      # It also collects any given named segment in the pattern that will be
+      # returned as parameter.
+      #
+      # Examples:
+      #
+      # * A simple path: `"/products"`
+      # * A path with a named segment: `"/products/:id"`
+      # * A path with multiple named segments: `"/products/:category/:id"`
       def initialize(@pattern)
         @names = [] of String
         @compiled = compile
@@ -19,6 +56,20 @@ module Beryl
         Regex.new("^#{prepared}$")
       end
 
+      # Returns a Hash with parameters extracted from given Request.
+      #
+      # Will combine both named segments and query parameters into the
+      # resulting Hash.
+      #
+      # Example:
+      #
+      # ```
+      # compiled = CompiledPattern.new("/products/:category")
+      # request  = HTTP::Request.new("GET", "/products/furniture?page=3&sort=date")
+      #
+      # params = compiled.extract_params(request)
+      # # => { "category" => "furniture", "page" => 3, "sort" => "date" }
+      # ```
       def extract_params(request)
         params = {} of String => String
 
@@ -32,6 +83,18 @@ module Beryl
         params
       end
 
+      # Returns true if `Request#path` matches compiled pattern.
+      #
+      # It will perform a comparison using `URI#path` to avoid any query
+      # parameter or malformed URL affect the comparison.
+      #
+      # ```
+      # compiled = CompiledPattern.new("/products")
+      # request  = HTTP::Request.new("GET", "/")
+      #
+      # compiled.matches?(request)
+      # # => false
+      # ```
       def matches?(request)
         @compiled.match(request.uri.path.to_s) ? true : false
       end
