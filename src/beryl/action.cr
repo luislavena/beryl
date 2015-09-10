8f7077d11f017964dab94a4170bc6ce8647e17e9
Luis Lavena <luislavena@gmail.com>
2015-09-10 19:05:43 -0300

    doc: Beryl::Action

--- src/beryl/action.cr
+++ src/beryl/action.cr
@@ -1,8 +1,21 @@
 module Beryl
+  # Actions are the endpoint that responds to incoming HTTP requests with
+  # proper HTTP responses.
+  #
+  # Example:
+  #
+  # ```
+  # class Hello < Beryl::Action
+  #   def call(params)
+  #     HTTP::Response.ok "text/plain", "Hello!"
+  #   end
+  # end
+  # ```
   abstract class Action
     macro inherited
       property :request
 
+      # :nodoc:
       def self.call(request, params)
         instance = new
         instance.request = request
@@ -10,6 +23,24 @@ module Beryl
       end
     end
 
+    # Returns an `HTTP::Response` as result of the action invocation.
+    #
+    # **Must** be implemented by you and is expected to take `params` (in the
+    # form of `Hash(String, String)`) and always return an `HTTP::Response`.
+    #
+    # Example:
+    #
+    # ```
+    # class ShowProduct < Beryl::Action
+    #   def call(params)
+    #     if params["id"] == "100"
+    #       HTTP::Response.ok "text/plain", "Found!"
+    #     else
+    #       HTTP::Response.not_found
+    #     end
+    #   end
+    # end
+    # ```
     abstract def call(params) : HTTP::Response
   end
 end
