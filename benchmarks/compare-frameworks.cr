57eb7a63d204a3413138a5980ca5c58b14dd6b3f
Luis Lavena <luislavena@gmail.com>
2015-11-17 12:04:49 -0300

    bench: add Kemal to comparison
        amethyst (req_root)  69.31k (± 0.53%) 43.57× slower
         artanis (req_root) 845.13k (± 1.26%)  3.57× slower
           beryl (req_root)   1.58M (± 1.39%)  1.91× slower
           kemal (req_root)   3.02M (± 2.31%)       fastest
    
        amethyst (req_id)   56.1k (± 0.72%) 54.23× slower
         artanis (req_id) 451.43k (± 2.26%)  6.74× slower
           beryl (req_id) 992.66k (± 3.68%)  3.06× slower
           kemal (req_id)   3.04M (± 2.00%)       fastest
    
        amethyst (req_middle)  38.24k (± 0.75%) 79.32× slower
         artanis (req_middle)  47.26k (± 0.58%) 64.19× slower
           beryl (req_middle) 804.19k (± 2.46%)  3.77× slower
           kemal (req_middle)   3.03M (± 0.94%)       fastest
    
        amethyst (req_bottom)  20.22k (± 0.92%) 147.50× slower
         artanis (req_bottom)  34.62k (± 1.59%)  86.13× slower
           beryl (req_bottom)   1.28M (± 1.10%)   2.34× slower
           kemal (req_bottom)   2.98M (± 0.83%)        fastest

--- benchmarks/compare-frameworks.cr
+++ benchmarks/compare-frameworks.cr
@@ -4,6 +4,7 @@ require "http/request"
 require "amethyst"
 require "artanis"
 require "../src/beryl"
+require "kemal/kemal/*" # avoid `at_exit`
 
 EMPTY_RESPONSE = ""
 MIME_HTML      = "text/html"
@@ -75,6 +76,28 @@ end
 
 beryl_app = BerylApp.new
 
+# Kemal
+
+class KemalApp < Kemal::Handler
+  get "/" do
+    HTTP::Response.ok MIME_HTML, EMPTY_RESPONSE
+  end
+  get "/posts/:id" do
+    HTTP::Response.ok MIME_HTML, EMPTY_RESPONSE
+  end
+  {% for idx in 1..100 %}
+      get "/comments/{{idx}}" do
+        HTTP::Response.ok MIME_HTML, EMPTY_RESPONSE
+      end
+  {% end %}
+
+  get "/bottom" do
+    HTTP::Response.ok MIME_HTML, EMPTY_RESPONSE
+  end
+end
+
+kemal_app = KemalApp.new
+
 # Benchmark
 
 req_root = HTTP::Request.new("GET", "/")
@@ -86,22 +109,26 @@ Benchmark.ips do |x|
   x.report("amethyst (req_root)") { amethyst_app.call(req_root) }
   x.report("artanis (req_root)") { ArtanisApp.call(req_root) }
   x.report("beryl (req_root)") { beryl_app.call(req_root) }
+  x.report("kemal (req_root)") { kemal_app.call(req_root) }
 end
 
 Benchmark.ips do |x|
   x.report("amethyst (req_id)") { amethyst_app.call(req_id) }
   x.report("artanis (req_id)") { ArtanisApp.call(req_id) }
   x.report("beryl (req_id)") { beryl_app.call(req_id) }
+  x.report("kemal (req_id)") { kemal_app.call(req_id) }
 end
 
 Benchmark.ips do |x|
   x.report("amethyst (req_middle)") { amethyst_app.call(req_middle) }
   x.report("artanis (req_middle)") { ArtanisApp.call(req_middle) }
   x.report("beryl (req_middle)") { beryl_app.call(req_middle) }
+  x.report("kemal (req_middle)") { kemal_app.call(req_middle) }
 end
 
 Benchmark.ips do |x|
   x.report("amethyst (req_bottom)") { amethyst_app.call(req_bottom) }
   x.report("artanis (req_bottom)") { ArtanisApp.call(req_bottom) }
   x.report("beryl (req_bottom)") { beryl_app.call(req_bottom) }
+  x.report("kemal (req_bottom)") { kemal_app.call(req_bottom) }
 end
